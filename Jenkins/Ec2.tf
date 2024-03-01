bd358b70269b641d562f3c332ca392ca976db97b
Roman Reings <Rajkumar>
2024-03-01 21:44:06 +0530

    Jenkins files

---
+++ Jenkins/Ec2.tf
@@ -0,0 +1,19 @@
+resource "aws_instance" "name" {
+  count = length(var.instance_names)
+  ami             = var.ami_id
+  instance_type   = "t2.micro"
+  security_groups = [aws_security_group.allow_all.name]
+  user_data       = var.instance_names[count.index] == "Jenkins" ? "${file("jenkins.sh")}" : "${file("agent.sh")}"
+  tags = merge({
+    Name = var.instance_names[count.index]
+  }, var.common_tags)
+}
+
+# resource "aws_instance" "name1" {
+#   ami             = var.ami_id
+#   instance_type   = "t2.micro"
+#   security_groups = [aws_security_group.allow_all.name]
+#   tags = merge({
+#     Name = var.instance_names[1]
+#   }, var.common_tags)
+# }
